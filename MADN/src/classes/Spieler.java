8f0a83111f6d255f419b91023016069d1c77b766
JohannesMaendle <Johannes.Maendle@Student.Reutlingen-University.de>
2015-03-10 19:40:05 +0100

    Würfel: Keine großen Änderungen, da Sebastian ja schon die eine Zeile geschrieben hat. Habe noch die JavaDoc-Kommentare hinzugefügt.
    Spieler: Erster Versuch der Klasse, jedoch fehlt noch die Möglichkeit,
    dass ein Spieler ein Objekt der Klasse KI ist, da diese ja noch nicht
    vorhanden ist.

---
+++ MADN/src/classes/Spieler.java
@@ -0,0 +1,39 @@
+package classes;
+/**
+ * Die Klasse Spieler
+ * @author Johannes Mändle
+ * @version 1.0
+ */
+
+public class Spieler {
+
+	private String name;
+	private FarbEnum farbe;
+	private Würfel würfel;
+	private Spielfigur fig_1;
+	private Spielfigur fig_2;
+	private Spielfigur fig_3;
+	private Spielfigur fig_4;
+
+	/**
+	 * Konstruktor
+	 * @param Name des Spielers
+	 * @param Farbe des Spielers
+	 */
+	public Spieler(String name, int farbe)
+	{
+		this.name = name;
+		switch(farbe)
+		{
+		case 0: this.farbe = FarbEnum.rot; break;
+		case 1: this.farbe = FarbEnum.blau; break;
+		case 2: this.farbe = FarbEnum.grün; break;
+		case 3: this.farbe = FarbEnum.gelb; break;
+		default: throw new RuntimeException("Farbe existiert nicht");
+		}
+		fig_1 = new Spielfigur(farbe);
+		fig_2 = new Spielfigur(farbe);
+		fig_3 = new Spielfigur(farbe);
+		fig_4 = new Spielfigur(farbe);
+	}
+}
--- MADN/src/classes/Würfel.java
+++ MADN/src/classes/Würfel.java
@@ -1,8 +1,16 @@
 package classes;
-
+/**
+ * Klasse Würfel
+ * @author Sebastian Stumm, Johannes Mändle
+ * @version 1.0
+ */
 public class Würfel {
 
-	public int werfen(){
+	/**
+	 * Funktion werfen()
+	 * @return Die Funktion liefert eine Ganzzahl zwischen 1 und 6 zurück
+	 */
+	public static int werfen(){
 		return (int)((Math.random()*6)+1);
 	}
 	
