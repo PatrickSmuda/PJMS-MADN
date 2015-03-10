72b8527de2dccffe2d2b6c131213e6c3fcbdc9a7
JohannesMaendle <Johannes.Maendle@Student.Reutlingen-University.de>
2015-03-10 20:08:05 +0100

    Die Klasse Spieler wieder dabei, jedoch werden die einzelnen Spielfiguren noch nicht initialisiert, da ich irgendwie Probleme mit dem Konstruktor der Spielfiguren habe..

--- MADN/src/classes/Spielfeld.java
+++ MADN/src/classes/Spielfeld.java
@@ -5,7 +5,7 @@ public class Spielfeld {
 	private Spielfigur figur;
 	private static int id;
 	private FeldTyp typ;
-	//private Spieler spielerFeld;
+	private Spieler spielerFeld;
 	private FarbEnum feldFarbe;
 	
 	public Spielfeld(FeldTyp typ, FarbEnum farbe){
