529619423644a85f89019cff56d63f10a01cd33b
SebastianStumm <Sebastian.Stumm@Student.Reutlingen-University.DE>
2015-03-10 16:09:30 +0100

    Spielfigur doc und Klasse v1.1

--- MADN/src/classes/Spielfigur.java
+++ MADN/src/classes/Spielfigur.java
@@ -6,14 +6,17 @@ package classes;
  */
 public class Spielfigur {
 	private FarbEnum farbe;
-	private int position;
+	private Spielfeld position;
 	
 	/**
-	 * Der Konstruktor setzt die Farbe der Figur
-	 * @param farbe Farbe der Figur
+	 * Der Konstruktor setzt die Farbe und Startposition der Figur
+	 * @param farbe Farbe der Spielfigur
+	 * @param position Spielfeld auf der die Spielfigur startet
 	 */
-	public Spielfigur(FarbEnum farbe){
+	public Spielfigur(FarbEnum farbe, Spielfeld position){
+		if(farbe == null || position == null) throw new RuntimeException("Ungültige Farbe oder Position der Spielfigur");
 		this.farbe = farbe;
+		this.position = position;
 	}
 	/**
 	 * Die Methode gibt die Farbe der Figur zurück
@@ -21,6 +24,11 @@ public class Spielfigur {
 	 */
 	public FarbEnum getFarbe(){
 		return this.farbe;
+	}
+	
+	public void bewegeAuf(Spielfeld position){
+		if(position == null) throw new RuntimeException("Spielfeld auf das die Spielfigur laufen soll existiert nicht");
+		this.position = position;
 	}
 
 }
