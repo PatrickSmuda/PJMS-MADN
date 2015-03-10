6fe60f1c0a90c47bddcece4d15b16488768c2284
SebastianStumm <Sebastian.Stumm@Student.Reutlingen-University.DE>
2015-03-10 15:58:09 +0100

    Die Farb Enumeration und die Spielfigur Klasse

--- MADN/src/classes/FarbEnum.java
+++ MADN/src/classes/FarbEnum.java
@@ -1,5 +1,9 @@
 package classes;
-
+/**
+ * Die Farb Enumeration
+ * @author WinWorkLap
+ * @version 1.0
+ */
 public enum FarbEnum {
 	rot,
 	blau,
--- MADN/src/classes/Spielfigur.java
+++ MADN/src/classes/Spielfigur.java
@@ -1,15 +1,24 @@
 package classes;
-
-//Test Kommentar
-
+/**
+ * Die Klasse Spielfigur
+ * @author Sebastian Stumm
+ * @version 1.0
+ */
 public class Spielfigur {
-	private FarbEnum farbe; // testtestblabla
+	private FarbEnum farbe;
 	private int position;
 	
+	/**
+	 * Der Konstruktor setzt die Farbe der Figur
+	 * @param farbe Farbe der Figut
+	 */
 	public Spielfigur(FarbEnum farbe){
 		this.farbe = farbe;
 	}
-	
+	/**
+	 * Die Methode gibt die Farbe der Figur zurück
+	 * @return farbe, die Farbe der Figur 
+	 */
 	public FarbEnum getFarbe(){
 		return this.farbe;
 	}
