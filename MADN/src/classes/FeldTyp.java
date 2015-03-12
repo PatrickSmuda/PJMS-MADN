09052c17b601fbbc0c79ef3f6d5cc8b3480af54a
PatrickSmuda <patrick.smuda@student.reutlingen-university.de>
2015-03-12 02:57:24 +0100

    Work-in-Progress Version der "Spielebrett" Klasse mit allen Feldern. Java Doc für die Klassen "Spielbrett" und "Feldtyp" (Wobei beim Feldtyp der Autor fehlt, da ich nicht genau weis wer diese geschrieben hat)

--- MADN/src/classes/FeldTyp.java
+++ MADN/src/classes/FeldTyp.java
@@ -1,5 +1,9 @@
 package classes;
-
+/**
+ * Die Feldtyp Enumeration
+ * @author ""
+ * @version 1.0
+ */
 public enum FeldTyp {
 	Normalfeld,
 	Startfeld,
--- MADN/src/classes/Spielbrett.java
+++ MADN/src/classes/Spielbrett.java
@@ -1,49 +1,64 @@
 package classes;
-
-public class Spielbrett {
+/**
+ * Klasse Spielbrett
+ * @author Patrick Smuda
+ * @version 0.1
+ */
+public class Spielbrett{
 	
-	private Spielfeld[] brett;
+	private Spielfeld[] spielbrett;
 	
-	public Spielbrett(){
-		brett = new Spielfeld[76];
+	/**
+	 * Der Konstruktor setzt das Spielfeld fest
+	 * @param typ Typ des Feldes
+	 * @param farbe Farbe des Feldes
+	 */
+	public Spielbrett(FeldTyp typ, FarbEnum farbe){
+	
+		spielbrett = new Spielfeld[72];
 		
-		for(int i = 0; i < 4; i++){
-			brett[i] = new Spielfeld(FeldTyp.Startfeld,FarbEnum.rot);
+		//Normale Spielfelder
+		for (int n = 0; n < 40; n++) {
+			spielbrett[n] = new Spielfeld(FeldTyp.Normalfeld, null);
 		}
-		for(int i = 4; i < 8; i++){
-			brett[i] = new Spielfeld(FeldTyp.Startfeld, FarbEnum.blau);
-		}
-		for(int i = 8; i < 12; i++){
-			brett[i] = new Spielfeld(FeldTyp.Startfeld, FarbEnum.grün);
-		}
-		for(int i = 12; i < 16; i++){
-			brett[i] = new Spielfeld(FeldTyp.Startfeld, FarbEnum.gelb);
-		}
-		for(int i = 16; i < 20; i++){
-			brett[i] = new Spielfeld(FeldTyp.Endfeld, FarbEnum.rot);
-		}
-		for(int i = 20; i < 24; i++){
-			brett[i] = new Spielfeld(FeldTyp.Endfeld, FarbEnum.blau);
-		}
-		for(int i = 24; i < 28; i++){
-			brett[i] = new Spielfeld(FeldTyp.Endfeld, FarbEnum.gelb);
-		}
-		for(int i = 28; i < 32; i++){
-			brett[i] = new Spielfeld(FeldTyp.Endfeld, FarbEnum.grün);
-		}
-		for(int i = 28; i < brett.length; i++){
-			brett[i] = new Spielfeld(FeldTyp.Normalfeld, null);
-		}
-		 
-		 
 		
-
-			
+		//Die Startfelder der Spieler
+		for (int n = 40; n < 44; n++) {
+			spielbrett[n] = new Spielfeld(FeldTyp.Startfeld, FarbEnum.blau);
+		}
+		for (int n = 44; n < 48; n++) {
+			spielbrett[n] = new Spielfeld(FeldTyp.Startfeld, FarbEnum.grün);
+		}
+		for (int n = 48; n < 52; n++) {
+			spielbrett[n] = new Spielfeld(FeldTyp.Startfeld, FarbEnum.gelb);
+		}
+		for (int n = 52; n < 56; n++) {
+			spielbrett[n] = new Spielfeld(FeldTyp.Startfeld, FarbEnum.rot);
+		}
 		
+		//Die Endfelder der Spieler
+		for(int n = 56; n < 60; n++){
+			spielbrett[n] = new Spielfeld(FeldTyp.Endfeld, FarbEnum.blau);
+		}
+		for(int n = 60; n < 64; n++){
+			spielbrett[n] = new Spielfeld(FeldTyp.Endfeld, FarbEnum.gelb);
+		}
+		for(int n = 64; n < 68; n++){
+			spielbrett[n] = new Spielfeld(FeldTyp.Endfeld, FarbEnum.grün);
+		}
+		for(int n = 68; n < 72; n++){
+			spielbrett[n] = new Spielfeld(FeldTyp.Endfeld, FarbEnum.rot);
+		}
 	}
-	public Spielfeld[] getBrett(){
-		return this.brett;
+	
+	
+	public Spielfeld[] getSpielbrett(){
+		return this.spielbrett;
 	}
-
+	
+	private void setSpielbrett(Spielfeld[] spielbrett) {
+		this.spielbrett = spielbrett;
+	}
 	
 }
+
