02b0e3c7876c66a22fb26abe71185382c45dd624
MersihaDervisevic <mersiha.dervisevic@student.reutlingen-university.de>
2015-03-12 12:30:24 +0100

    //

--- MADN/src/classes/Spielfeld.java
+++ MADN/src/classes/Spielfeld.java
@@ -9,12 +9,12 @@ public class Spielfeld {
 	private FarbEnum feldFarbe;
 	
 	
-	public Spielfeld(FeldTyp typ, FarbEnum farbe){
+	public Spielfeld(FeldTyp typ, FarbEnum farbe, int id){
 		switch(typ)
 		{
-		case Normalfeld: this.typ = FeldTyp.Normalfeld; figur = null; feldFarbe = null; id++; break;
-		case Startfeld: this.typ = FeldTyp.Startfeld; figur = null; feldFarbe = figur.getFarbe(); id++; break;
-		case Endfeld: this.typ = FeldTyp.Endfeld;  figur = null; feldFarbe = farbe; id++; break;
+		case Normalfeld: this.typ = FeldTyp.Normalfeld; figur = null; feldFarbe = null;  break;
+		case Startfeld: this.typ = FeldTyp.Startfeld; figur = null; feldFarbe = figur.getFarbe();  break;
+		case Endfeld: this.typ = FeldTyp.Endfeld;  figur = null; feldFarbe = farbe;  break;
 		default: throw new RuntimeException("Kein gültiges Feld konnte erstellt werden!");
 		}
 	}
