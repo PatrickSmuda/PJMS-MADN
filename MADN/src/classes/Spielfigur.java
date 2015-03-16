ed4ffa8b16657e80b343b2ec7814873f1df5f05e
SebastianStumm <Sebastian.Stumm@Student.Reutlingen-University.DE>
2015-03-16 14:26:21 +0100

    Methoden geschlagen und bewegeAuf ausgekommentiert

--- MADN/src/classes/Spielfigur.java
+++ MADN/src/classes/Spielfigur.java
@@ -22,7 +22,7 @@ public class Spielfigur {
 	public Spielfigur(FarbEnum farbe, Spielfeld position){
 		if(farbe == null || position == null || position.getFigur() != null) throw new RuntimeException("Ungültige Farbe oder Position der Spielfigur");
 		this.farbe = farbe;
-		this.position = position;
+		this.startPosition = position;
 		switch(this.farbe)
 		{
 		case rot: if(this.anz_rot >= 4)throw new RuntimeException("Mehr als 4 Figuren sind nicht erlaubt!"); this.anz_rot++; break;
@@ -37,29 +37,21 @@ public class Spielfigur {
 	 */
 	public FarbEnum getFarbe(){
 		return this.farbe;
-	}
+	}/*
 	/**
 	 * Die Methode setzt die Spielfigur auf das Startfeld zurück
 	 */
-	public void geschlagen(){
-		switch(this.farbe)
-		{
-		case rot: this.position = this.startPosition; break;
-		case blau: break;
-		case grün: break;
-		case gelb: break;
-		default: throw new RuntimeException("Farbe der Figur nicht vorhanden");
-		}
-		
+	/*public void geschlagen(){
+		this.position = startPosition;
 	}
 	/**
 	 * Methode zum bewegen der Spielfigur
 	 * @param position Position auf welche die Spielfigur zieht
 	 */
-	public void bewegeAuf(Spielfeld position){
+	/*public void bewegeAuf(Spielfeld position){
 		if(position == null) throw new RuntimeException("Spielfeld auf das die Spielfigur laufen soll existiert nicht");
 		if(position.getFigur() != null) position.getFigur().geschlagen();
 		this.position = position;
-	}
+	}*/
 
 }
