8647fd1a469e4f82a476c15984ddd75e4f75f7c3
PatrickSmuda <patrick.smuda@student.reutlingen-university.de>
2015-03-16 15:21:39 +0100

    JUnit test ma wieder n neuer

---
+++ MADN/src/classes/TestSpielbrett.java
@@ -0,0 +1,19 @@
+package classes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSpielbrett {
+	protected Spielbrett s;
+	
+	
+	@Test
+	public void test() {
+//		if (condition) {
+//			fail("Not yet implemented");
+//		}
+		
+	}
+
+}
