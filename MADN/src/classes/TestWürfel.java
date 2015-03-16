c39baeee330888ac89973a945bc3bd66791c1a9b
PatrickSmuda <patrick.smuda@student.reutlingen-university.de>
2015-03-16 15:09:55 +0100

    Problemlösung

--- MADN/src/classes/TestWürfel.java
+++ MADN/src/classes/TestWürfel.java
@@ -5,12 +5,12 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestWürfel {
-	protected classes.Würfel w;
+	protected Würfel w;
 
 	@Test
 	public void test() {
 		for (int i = 0; i < 1000; i++) {
-			if (Würfel.werfen() < 1 || Würfel.werfen() > 6) {
+			if (w.werfen() < 1 || w.werfen() > 6) {
 				fail("Würfel soll nur 1-6 ausgeben");
 			}
 		}
