a70f422b7ddd1a6287968493b3cfa71a29e6942a
JohannesMaendle <Johannes.Maendle@Student.Reutlingen-University.de>
2015-03-17 11:31:29 +0100

    Komposition in der Klasse KI

--- MADN/src/classes/KI.java
+++ MADN/src/classes/KI.java
@@ -6,11 +6,14 @@ package classes;
  */
 
 class KI {
+
+	private Spieler spieler;
 	/**
 	 * Konstruktor
 	 */
-	public KI(){
-		
-	}
+	public KI(Spieler spieler){
+		if (spieler == null) throw new RuntimeException("Spieler muss vorhanden sein!");	
+		this.spieler = spieler;
+	} 
 
 }
--- MADN/src/classes/test.java
+++ MADN/src/classes/test.java
@@ -4,8 +4,8 @@ public class test {
 
 	public static void main(String[] args) {
 	Spielbrett dasBrett = new Spielbrett();
-	Spieler spielerRot = new Spieler("Hugo", FarbEnum.rot, new KI(),dasBrett);
-	System.out.println(spielerRot.getWürfel().werfen());
+	//Spieler spielerRot = new Spieler("Hugo", FarbEnum.rot, new KI(),dasBrett);
+	//System.out.println(spielerRot.getWürfel().werfen());
 	}
 
 }
