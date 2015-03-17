4726cb356f0dbee67fea2378d0dc18c4fd35ae03
SebastianStumm <Sebastian.Stumm@Student.Reutlingen-University.DE>
2015-03-17 10:08:46 +0100

    TrivialFall raus und genaue Fehlerbeschreibung

--- MADN/src/classes/SpielfigurTests.java
+++ MADN/src/classes/SpielfigurTests.java
@@ -1,20 +1,26 @@
 package classes;
-
+/**
+ * Die Klasse zum testen der Funktionalität der Spielfigur
+ * @author Sebastian Stumm
+ * @version 1.0
+ */
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-
+/**
+ * Prüffälle
+ */
 public class SpielfigurTests {
 
-	Spielfigur sf;
-	Spielbrett sb;
+	private Spielfigur sf;
+	private Spielbrett sb;
 	
 	@Test
 	public void test() {
 		sb = new Spielbrett();
 		sf = new Spielfigur(FarbEnum.rot,sb.getFeld(25));
-		if(sf.getFarbe()==null)fail("Not yet implemented");
-		if(sf.getPosition()==null)fail("Not yet implemented");
+		//if(sf.getFarbe()==null)fail("Farbe der Figur ist null");
+		if(sf.getPosition()==null)fail("Figur steht auf keinem Feld");
 	}
 
 }
