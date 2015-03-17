b567b81412b4408811c521995a3e7edee59ec54d
PatrickSmuda <patrick.smuda@student.reutlingen-university.de>
2015-03-17 12:21:56 +0100

    Doc zu Testwürfel hinzugefügt

--- MADN/src/classes/TestWürfel.java
+++ MADN/src/classes/TestWürfel.java
@@ -1,12 +1,18 @@
 package classes;
-
+/**
+ * Die Klasse zum testen der Funktionalität des Würfels
+ * @author Patrick Smuda
+ * @version 1.0
+ */
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class TestWürfel {
 	protected Würfel w;
-
+	/**
+	 * Prüffall für das würfeln eines 6-Seitigen Würfels
+	 */
 	@Test
 	public void test() {
 		for (int i = 0; i < 1000; i++) {
