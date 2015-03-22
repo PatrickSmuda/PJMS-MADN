2bb29271ae8ecdaf9a462d7db3ee1680e12395eb
PatrickSmuda <patrick.smuda@student.reutlingen-university.de>
2015-03-22 16:27:09 +0100

    Arbeit an TestSpiel

--- MADN/src/classes/TestSpiel.java
+++ MADN/src/classes/TestSpiel.java
@@ -5,10 +5,18 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestSpiel {
-
+	protected Spielbrett s;
+	protected Wuerfel w;
+	protected Spieler s1;
+	protected Spieler s2;
+	protected Spieler s3;
+	protected Spieler s4;
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
 	}
+	
+	
 
 }
