9969310a19859aadd28c2d6f29264b235806adea
PatrickSmuda <patrick.smuda@student.reutlingen-university.de>
2015-03-22 18:46:46 +0100

    SpielTest änderungen

--- MADN/src/classes/SpielTest.java
+++ MADN/src/classes/SpielTest.java
@@ -1,11 +1,17 @@
 package classes;
 
 public class SpielTest {
-
+	
+	Spielbrett sb = new Spielbrett();
+	Spieler s1 = new Spieler("hans", FarbEnum.rot, null, sb);
+	Spieler s2 = new Spieler("gretel", FarbEnum.blau, null, sb);
+	Spieler s3 = new Spieler("blub", FarbEnum.gruen, null, sb);
+	Spieler s4 = new Spieler("blaaa", FarbEnum.gelb, null, sb);
+	
 	public static void main(String[] args) {
 
-		iBediener s = new Spiel(null, null, null, null);
 		
+		s.bewege(null);
 		
 	}
 }
