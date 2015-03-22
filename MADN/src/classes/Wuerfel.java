9f73bfcb2b50839f732d9e9dd297d8fdf15743d8
SebastianStumm <Sebastian.Stumm@Student.Reutlingen-University.DE>
2015-03-22 18:02:47 +0100

    static weg

--- MADN/src/classes/Wuerfel.java
+++ MADN/src/classes/Wuerfel.java
@@ -17,7 +17,7 @@ public class Wuerfel {
 	 * Funktion werfen()
 	 * @return Die Funktion liefert eine Ganzzahl zwischen 1 und 6 zurueck
 	 */
-	public static int werfen(){
+	public int werfen(){
 		return (int)((Math.random()*6)+1);
 	}
 	
