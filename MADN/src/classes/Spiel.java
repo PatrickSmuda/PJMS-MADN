5128810c9d3f529f14f369f225b56ca1ae376220
JohannesMaendle <Johannes.Maendle@Student.Reutlingen-University.de>
2015-03-22 17:16:43 +0100

    Spiel Klasse

--- MADN/src/classes/Spiel.java
+++ MADN/src/classes/Spiel.java
@@ -3,18 +3,16 @@ package classes;
 public class Spiel {
 
 	private Spielbrett spielbrett;
-	private Spieler s1;
-	private Spieler s2;
-	private Spieler s3;
-	private Spieler s4;
+	private Spieler[] spieler;
 	private Spieler istAmZug;
 	
 	public Spiel(Spieler s1, Spieler s2, Spieler s3, Spieler s4){
-		if (s1 == null) throw new RuntimeException("Ungültige Eingabe");
-		this.s1 = s1;
-		this.s2=s2;
-		this.s3=s3;
-		this.s4=s4;
+		if (s1 == null || s2 == null) throw new RuntimeException("Ungültige Eingabe");
+		spieler = new Spieler[4];
+		spieler[0] = s1;
+		spieler[1] = s2;
+		spieler[2] = s3;
+		spieler[3] = s4;
 	}
 	
 	/**
