62d566bca73093d907020c080e111f2b14d3c959
PatrickSmuda <patrick.smuda@student.reutlingen-university.de>
2015-03-22 16:17:31 +0100

    JUnit Test zum Spiel

---
+++ MADN/src/classes/TestSpiel.java
@@ -0,0 +1,14 @@
+package classes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSpiel {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
