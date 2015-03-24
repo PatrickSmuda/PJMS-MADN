4ab32ef2313ae5be7dda77efb86ccfad76195d5b
MersihaDervisevic <mersiha.dervisevic@student.reutlingen-university.de>
2015-03-24 12:24:08 +0100

    tfghjnk

--- MADN/src/classes/SpielTest.java
+++ MADN/src/classes/SpielTest.java
@@ -15,7 +15,7 @@ public class SpielTest {
 		Spieler s2 = new Spieler("gretel", FarbEnum.blau, null, sb);
 		Spieler s3 = new Spieler("hanni", FarbEnum.gruen, null, sb);
 		Spieler s4 = new Spieler("nanni", FarbEnum.gelb, null, sb);
-		Spiel s = new Spiel(s1, s2, s3, s4);
+		Spiel s = new Spiel();
 		
 		iBediener p;
 		
