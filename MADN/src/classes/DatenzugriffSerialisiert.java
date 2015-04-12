03f9871127b8680cd0d402269a2315104c41e014
MersihaDervisevic <mersiha.dervisevic@student.reutlingen-university.de>
2015-04-12 17:27:01 +0200

    fghjk

--- MADN/src/classes/DatenzugriffSerialisiert.java
+++ MADN/src/classes/DatenzugriffSerialisiert.java
@@ -1,32 +1,11 @@
 package classes;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-
-public class DatenzugriffSerialisiert implements iDatenzugriff {
+public class DatenzugriffSerialisiert implements iDatenzugriff  {
 
 	
 	
-	ObjectOutputStream oos = null;
-	
-	public void speichern(Spiel spiel){
-		try{
-			oos = new ObjectOutputStream(new FileOutputStream("out.ser"));
-			oos.writeObject(spiel);
-		} catch(FileNotFoundException e){
-			System.err.println("Datei konnte nicht erstellt werden!");
-		} catch (IOException e) {
-			System.err.println("Fehler bei Ein-/Ausgabe: " + e);
-		}
-		finally{
-			try{
-				oos.close();
-			}catch(Exception e){
-				System.err.println("Fehler beim Schlieﬂen!");
-			}
-		}
+	public void speichern(){
+		
 	}
 	
 	public void laden(){ 
