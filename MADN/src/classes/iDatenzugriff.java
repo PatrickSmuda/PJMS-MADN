8878233a1a19c6feb28a076534746798c2bc471f
MersihaDervisevic <mersiha.dervisevic@student.reutlingen-university.de>
2015-04-14 13:59:51 +0200

    kommentare bla

--- MADN/src/classes/iDatenzugriff.java
+++ MADN/src/classes/iDatenzugriff.java
@@ -1,8 +1,14 @@
 package classes;
-
+/**
+ * 
+ * @author 
+ * @version 1.0
+ * Das Interface iDatenzugriff
+ * 
+ */
 public interface iDatenzugriff {
 
-	public void speichern(Spieler[] spieler);
+	public void speichern(Spiel spiel);
 	
 	public void laden();
 	
