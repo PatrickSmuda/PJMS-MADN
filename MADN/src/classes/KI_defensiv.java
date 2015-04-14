b8e0696c8019a6e578b799d8ea1f81e49486b236
MersihaDervisevic <mersiha.dervisevic@student.reutlingen-university.de>
2015-04-14 13:40:31 +0200

    blaaaaaaaaaaaaaaaa

---
+++ MADN/src/classes/KI_defensiv.java
@@ -0,0 +1,79 @@
+package classes;
+
+/**
+ * Die Defensive KI-Klasse
+ * @author Sebastian Stumm
+ * @version 1.0
+ */
+
+public class KI_defensiv extends KI{
+	
+	private iBediener p;
+	private boolean repeat = false;
+	private boolean bewegt = false;
+	
+	/**
+	 * Der Konstruktor der Defensiven KI
+	 * @param spieler der Spieler mit der KI
+	 * @param spiel das Spiel an der die KI teil nimmt
+	 */
+	
+	public KI_defensiv(Spieler spieler, Spiel spiel){
+		super(spieler, spiel);
+		p = spiel;
+	}
+	
+	/**
+	 * Die Methode die den eigentlichen Spielzug der KI durchführt
+	 */
+	@Override
+	public void kalkuliereSpielzug() {
+		
+		do{
+			p.wuerfeln();
+			//ASDF
+			//System.out.println(p.getBewegungsWert());
+			if(bewegt == false && geheAufEndfeld()){
+				if(p.getBewegungsWert() == 6){
+					bewegt = true;
+					repeat = true;
+				}else{
+					bewegt = false;
+					repeat = false;
+					break;
+				}
+			}else if(bewegt == false && betreteSpielfeld()){
+				if(p.getBewegungsWert() == 6){
+					bewegt = true;
+					repeat = true;
+				}else{
+					bewegt = false;
+					repeat = false;
+					break;
+				}
+			}else if(bewegt == false &&schlageGegner()){
+				if(p.getBewegungsWert() == 6){
+					bewegt = true;
+					repeat = true;
+				}else{
+					bewegt = false;
+					repeat = false;
+					break;
+				}
+			}else if(bewegt == false && laufEinfach()){
+				if(p.getBewegungsWert() == 6){
+					bewegt = true;
+					repeat = true;
+				}else{
+					bewegt = false;
+					repeat = false;
+					break;
+				}
+			}
+			System.out.println(p.getPosition(1));
+			bewegt = false;
+		}while(repeat);
+		
+	}
+	
+}
