5b3d7aabf506c8cf85d175ce9920265420e00240
MersihaDervisevic <mersiha.dervisevic@student.reutlingen-university.de>
2015-05-03 16:08:47 +0200

    mail

---
+++ MADN/src/GUI/MailTest.java
@@ -0,0 +1,13 @@
+package GUI;
+
+public class MailTest {
+
+	public static void main (String [] args){ 
+		new Mail("mersiha.d@gmx.de", "penis", "peeeenis",
+				"d:/javamail.ppt", "folien.ppt", null, null);
+		
+		System.out.println("ENDEEEE");
+		
+	}
+	
+}
