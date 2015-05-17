c4a6ab7b4a6589d2bb586bd2fef76cb942169a62
MersihaDervisevic <mersiha.dervisevic@student.reutlingen-university.de>
2015-05-17 14:14:19 +0200

    blablub

--- MADN/src/GUI/MailTest.java
+++ MADN/src/GUI/MailTest.java
@@ -3,7 +3,7 @@ package GUI;
 public class MailTest {
 
 	public static void main (String [] args){ 
-		new Mail("mersiha.d@gmx.de", "penis", "peeeenis",
+		new Mail("domz131@freenet.de", "penis", "peeeenis",
 				"C:\\Users\\Mersiha\\Documents\\hochschule\\semester 2\\MMI\\Persona.pdf", "penis.pdf" ,null, null);
 		
 		System.out.println("ENDEEEE");//vgvmhbnvhmvghvgk
